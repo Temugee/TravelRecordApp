67334e9758c5e6f4d168237284e6d92e22de68b9
Temuge <temuge.erdene@gmail.com>
2024-04-05 10:14:31 +0800

    mapPage, google map sdk applied.

--- TravelRecordApp/TravelRecordApp/MapPage.xaml.cs
+++ TravelRecordApp/TravelRecordApp/MapPage.xaml.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using Plugin.Geolocator;
+using Plugin.Geolocator.Abstractions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
+using Xamarin.Forms.Maps;
 using Xamarin.Forms.Xaml;
 
 namespace TravelRecordApp
@@ -12,9 +15,55 @@ namespace TravelRecordApp
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class MapPage : ContentPage
     {
+        IGeolocator locator = CrossGeolocator.Current;
         public MapPage()
         {
             InitializeComponent();
+        }
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            GetLocation();
+        }
+        protected override void OnDisappearing()
+        {
+            locator.StopListeningAsync();
+        }
+
+        private async void GetLocation()
+        {
+            var status = await CheckAndRequestLocationPermission();
+            if (status == PermissionStatus.Granted)
+            {
+                var location = await Geolocation.GetLocationAsync();
+                
+                locator.PositionChanged += Locator_PositionChanged;
+                await locator.StartListeningAsync(new TimeSpan(0,1,0), 100);
+                locationsMap.IsShowingUser = true;
+                CenterMap(location.Latitude, location.Longitude);
+            }
+        }
+
+        private void Locator_PositionChanged(object sender, Plugin.Geolocator.Abstractions.PositionEventArgs e)
+        {
+            CenterMap(e.Position.Latitude, e.Position.Longitude);
+        }
+
+        private void CenterMap(double latitude, double longitude)
+        {
+            Position center = new Position(latitude, longitude);
+            MapSpan span = new MapSpan(center, 1, 1);
+            locationsMap.MoveToRegion(span);
+        }
+
+        private async Task<PermissionStatus> CheckAndRequestLocationPermission()
+        {
+            var status = await Permissions.CheckStatusAsync<Permissions.LocationWhenInUse>();
+            if (status == PermissionStatus.Granted)
+                return status;
+
+            status = await Permissions.RequestAsync<Permissions.LocationWhenInUse>();
+            return status;
         }
     }
 }
