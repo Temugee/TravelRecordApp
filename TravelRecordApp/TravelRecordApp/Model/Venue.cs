cb63561cc9814ae7daf5e6e7d5d769b71c3c6b81
Temuge <temuge.erdene@gmail.com>
2024-04-10 15:46:14 +0800

    noid50say

--- TravelRecordApp/TravelRecordApp/Model/Venue.cs
+++ TravelRecordApp/TravelRecordApp/Model/Venue.cs
@@ -5,27 +5,58 @@ using TravelRecordApp.Helpers;
 
 namespace TravelRecordApp.Model
 {
-    
+
+    public class Icon
+    {
+        public string prefix { get; set; }
+        public string suffix { get; set; }
+    }
+
     public class Category
     {
         public int id { get; set; }
         public string name { get; set; }
         public string short_name { get; set; }
         public string plural_name { get; set; }
+        public Icon icon { get; set; }
     }
 
+    public class Main
+    {
+        public double latitude { get; set; }
+        public double longitude { get; set; }
+    }
+
+    public class Geocodes
+    {
+        public Main main { get; set; }
+    }
 
     public class Location
     {
         public string address { get; set; }
         public string country { get; set; }
-        public string cross_street { get; set; }
         public string formatted_address { get; set; }
         public string locality { get; set; }
         public string postcode { get; set; }
         public string region { get; set; }
+        public string cross_street { get; set; }
     }
 
+    
+
+    public class Parent
+    {
+        public string fsq_id { get; set; }
+        public IList<Category> categories { get; set; }
+        public string name { get; set; }
+    }
+
+    public class RelatedPlaces
+    {
+        public IList<object> children { get; set; }
+        public Parent parent { get; set; }
+    }
 
     public class Result
     {
@@ -34,16 +65,43 @@ namespace TravelRecordApp.Model
         public IList<object> chains { get; set; }
         public string closed_bucket { get; set; }
         public int distance { get; set; }
+        public Geocodes geocodes { get; set; }
         public string link { get; set; }
         public Location location { get; set; }
         public string name { get; set; }
+        public RelatedPlaces related_places { get; set; }
         public string timezone { get; set; }
     }
+
+    public class Center
+    {
+        public double latitude { get; set; }
+        public double longitude { get; set; }
+    }
+
+    public class Circle
+    {
+        public Center center { get; set; }
+        public int radius { get; set; }
+    }
+
+    public class GeoBounds
+    {
+        public Circle circle { get; set; }
+    }
+
+    public class Context
+    {
+        public GeoBounds geo_bounds { get; set; }
+    }
+
+    
 
 
     public class VenueRoot
     {
         public IList<Result> results { get; set; }
+        public Context context { get; set; }
         public static string GenerateURL(double latitude, double longitude)
         {
             return string.Format(Constants.VENUE_SEARCH, latitude, longitude);
--- TravelRecordApp/TravelRecordApp/NewTravelPage.xaml.cs
+++ TravelRecordApp/TravelRecordApp/NewTravelPage.xaml.cs
@@ -1,53 +1,89 @@
-﻿using Plugin.Geolocator;
-using SQLite;
+﻿using SQLite;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using TravelRecordApp.Logic;
 using TravelRecordApp.Model;
 using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
+using Plugin.Geolocator;
+using System.Linq;
 
 namespace TravelRecordApp
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class NewTravelPage : ContentPage
-	{
-		public NewTravelPage ()
-		{
-			InitializeComponent ();
-		}
+    public partial class NewTravelPage : ContentPage
+    {
+        public Result selectedVenue;
+        public Post newPost;
+
+        public NewTravelPage()
+        {
+            InitializeComponent();
+            venueListView.ItemSelected += VenueListView_ItemSelected;
+        }
+
         protected override async void OnAppearing()
         {
             base.OnAppearing();
 
-            var locator = CrossGeolocator.Current;
-            var position = await locator.GetPositionAsync();
-            var venues = await VenueLogic.GetVenues(position.Latitude, position.Longitude);
+            try
+            {
+                var locator = CrossGeolocator.Current;
+                var position = await locator.GetPositionAsync();
+
+                var venues = await VenueLogic.GetVenues(position.Latitude, position.Longitude);
+                venueListView.ItemsSource = venues.results;
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Error", "Failed to load venues: " + ex.Message, "OK");
+            }
         }
 
-        private void ToolbarItem_Clicked(object sender, EventArgs e)
-		{
-			Post post = new Post()
-			{
-				Experience = experienceEntry.Text
-			};
-			using (SQLiteConnection conn = new SQLiteConnection(App.DatabaseLocation))
-			{
-                conn.CreateTable<Post>();
-                int rows = conn.Insert(post);
-                if (rows > 0)
+        private void VenueListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            try
+            {
+                selectedVenue = e.SelectedItem as Result;
+                newPost = new Post()
                 {
-                    DisplayAlert("Success", "Experience successfully inserted", "OK");
+                    Experience = experienceEntry.Text,
+                    VenueName = selectedVenue.name
+                };
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Error", "Failed to select venue: " + ex.Message, "OK");
+            }
+        }
+
+        private async void ToolbarItem_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                if (selectedVenue != null)
+                {
+                    using (SQLiteConnection conn = new SQLiteConnection(App.DatabaseLocation))
+                    {
+                        conn.CreateTable<Post>();
+                        int rows = conn.Insert(newPost);
+                        if (rows > 0)
+                        {
+                            await DisplayAlert("Success", "Experience successfully inserted", "OK");
+                        }
+                        else
+                        {
+                            await DisplayAlert("Failure", "Experience insertion failed", "OK");
+                        }
+                    }
                 }
                 else
                 {
-                    DisplayAlert("Failure", "Experience successfully inserted", "OK");
+                    await DisplayAlert("Error", "Please select a venue", "OK");
                 }
             }
-
+            catch (Exception ex)
+            {
+                await DisplayAlert("Error", "Failed to insert experience: " + ex.Message, "OK");
+            }
         }
     }
 }
