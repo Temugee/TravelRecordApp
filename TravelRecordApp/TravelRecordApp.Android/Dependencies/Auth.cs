1c4aba9787df0c1380c3de8a86c8aa8fca5d23b5
Temuge <temuge.erdene@gmail.com>
2024-04-15 16:55:43 +0800

    bypassing firebase app completed?

---
+++ TravelRecordApp/TravelRecordApp.Android/Dependencies/Auth.cs
@@ -0,0 +1,20 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TravelRecordApp.Helpers;
+
+namespace TravelRecordApp.Droid.Dependencies
+{
+    public class Auth
+    {
+        public Auth() { }
+        
+    }
+}
