0fa1a5e6ee4bde06ee5009ff7ec50865e2c6586f
Temuge <temuge.erdene@gmail.com>
2024-04-15 12:14:58 +0800

    firebase dawning

--- TravelRecordApp/TravelRecordApp/ProfilePage.xaml.cs
+++ TravelRecordApp/TravelRecordApp/ProfilePage.xaml.cs
@@ -24,6 +24,18 @@ namespace TravelRecordApp
             using (SQLiteConnection conn = new SQLiteConnection(App.DatabaseLocation))
             {
                 var postTable = conn.Table<Post>().ToList();
+                var categories = (from p in postTable
+                                  orderby p.CategoryId
+                                  select p.CategoryName).ToList();
+                Dictionary<string, int> categoriesCount = new Dictionary<string, int>();
+                foreach(var category in categories)
+                {
+                    var count = (from post in postTable
+                                 where post.CategoryName == category
+                                 select post).ToList().Count;
+                    categoriesCount.Add(category, count);
+                }
+                categoriesListView.ItemsSource = categoriesCount;
                 postCountLabel.Text = postTable.Count.ToString();
             }
         }
